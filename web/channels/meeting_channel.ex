72bff2a4dfd38cdd10fd1ce07655a8be1f73e6ca
Kyle Bolton <kyle.bolton@gmail.com>
2016-02-13 20:39:06 -0500

    Move agent start to join function and rename setup -> decode

--- web/channels/meeting_channel.ex
+++ web/channels/meeting_channel.ex
@@ -11,19 +11,19 @@ defmodule TalkingStick.MeetingChannel do
     "relinquish_stick"
   ]
 
-  def setup(json) do
+  def decode(json) do
     %{:meeting_id => meeting_id, :user => user} =
       Poison.decode!(json, as: %{user: %User{}}, keys: :atoms!)
 
-    meeting_id = String.to_atom(meeting_id)
-    MeetingAgent.start_link(meeting_id)
-    [meeting_id, user]
+    [String.to_atom(meeting_id), user]
   end
 
-  # handles any other subtopic as the room ID, for example `"rooms:12"`, `"rooms:34"`
+  # handles any other subtopic as the meeting ID, for example `"meetings:12"`, `"meetings:34"`
   # TODO Do we want this to "auth" the meeting request?
+  # TODO Should move decode/1 from all the handle_in/3 to here. Will require client changes to pass through some things in the auth_message
   def join("meetings:" <> meeting_id, auth_message, socket) do
     if authorized?(meeting_id, auth_message) do
+      MeetingAgent.start_link(String.to_atom(meeting_id))
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -31,7 +31,7 @@ defmodule TalkingStick.MeetingChannel do
   end
 
   def handle_in("sync", json, socket) do
-    # TODO: This is a bit different than setup(json). Dry it up?
+    # TODO: This is a bit different than decode(json). Dry it up?
     %{:meeting_id => meeting_id} = Poison.decode!(json, keys: :atoms!)
     meeting_id = String.to_atom(meeting_id)
     MeetingAgent.start_link(meeting_id)
@@ -42,7 +42,7 @@ defmodule TalkingStick.MeetingChannel do
   end
 
   def handle_in("become_moderator", json, socket) do
-    [meeting_id, user] = setup(json)
+    [meeting_id, user] = decode(json)
 
     {:ok, meeting} = MeetingAgent.become_moderator(meeting_id, user)
     broadcast! socket, "meeting", %{meeting: meeting}
@@ -50,7 +50,7 @@ defmodule TalkingStick.MeetingChannel do
   end
 
   def handle_in("relinquish_moderator", json, socket) do
-    [meeting_id, user] = setup(json)
+    [meeting_id, user] = decode(json)
 
     {:ok, meeting} = MeetingAgent.relinquish_moderator(meeting_id, user)
     broadcast! socket, "meeting", %{meeting: meeting}
@@ -58,7 +58,7 @@ defmodule TalkingStick.MeetingChannel do
   end
 
   def handle_in("reset_speaker_and_queue", json, socket) do
-    [meeting_id, user] = setup(json)
+    [meeting_id, user] = decode(json)
 
     {:ok, meeting} = MeetingAgent.reset_speaker_and_queue(meeting_id, user)
     broadcast! socket, "meeting", %{meeting: meeting}
@@ -66,7 +66,7 @@ defmodule TalkingStick.MeetingChannel do
   end
 
   def handle_in("request_stick", json, socket) do
-    [meeting_id, user] = setup(json)
+    [meeting_id, user] = decode(json)
 
     {:ok, meeting} = MeetingAgent.request_stick(meeting_id, user)
     broadcast! socket, "meeting", %{meeting: meeting}
@@ -74,7 +74,7 @@ defmodule TalkingStick.MeetingChannel do
   end
 
   def handle_in("unrequest_stick", json, socket) do
-    [meeting_id, user] = setup(json)
+    [meeting_id, user] = decode(json)
 
     {:ok, meeting} = MeetingAgent.unrequest_stick(meeting_id, user)
     broadcast! socket, "meeting", %{meeting: meeting}
@@ -82,7 +82,7 @@ defmodule TalkingStick.MeetingChannel do
   end
 
   def handle_in("relinquish_stick", json, socket) do
-    [meeting_id, user] = setup(json)
+    [meeting_id, user] = decode(json)
 
     {:ok, meeting} = MeetingAgent.relinquish_stick(meeting_id, user)
     broadcast! socket, "meeting", %{meeting: meeting}
